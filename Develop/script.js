5e5d66333827e42dcadc0bb57b1f9500e61a6c7b
JMADA257 <jadams1812@hotmail.com>
2023-09-01 10:01:41 -0600

    Updated notes to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
-
+//VARAIBLE DECLERATIONS
 $(function () {
-  var paraDate = $("#currentDay");
+  //
+  var pDate = $("#currentDay");
   var date = dayjs().format("dddd, MMMM D, YYYY h:mm A");
-  paraDate.text(date);
+  pDate.text(date);
 
   var currentHour = dayjs().format("H");
 
@@ -17,24 +18,35 @@ $(function () {
     }
   }
 
-
+  // variable for the button element for each div
   var SaveItem = $(".saveBtn");
+  //taking the items stored in local storage and parsing them basically turning them back into a js string
   var thingsToDo = JSON.parse(localStorage.getItem("events")) || {};
 
+  //Making an event listener with a click property so when someone clicks the save button this will happen
   SaveItem.on("click", storeEvents);
-
+  //
   for (const key in thingsToDo) {
+    //
     var timeBlockB = $("#" + key).children()[1];
+    //
     timeBlockB.value = thingsToDo[key];
+    //
     console.log(timeBlockB);
   }
-
+  //creating a function to store events
   function storeEvents(event) {
+    //making an object array consisting of two different values
     var eventStore = {
+      //This will select the siblings with and specifically the sibling with an index of 1 and withdraw the value from it
       event: $(this).siblings()[1].value,
+      //This will select the parent of the "this" which is the div. This referering to the button and grabbing the hour of the day and saving it basically as the key
       time: $(this).parent().attr("id"),
     };
+
+    //
     thingsToDo[eventStore.time] = eventStore.event;
+    //
     localStorage.setItem("events", JSON.stringify(thingsToDo));
   }
 });
