38bb414151df92741c0fc376ed2215a6dd3f444e
JMADA257 <jadams1812@hotmail.com>
2023-09-03 19:10:15 -0600

    added notes to JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,26 @@
 //VARAIBLE DECLERATIONS
 $(function () {
-  //
+  //setting up a variable that links to the paragraph section with the class of current day 
   var pDate = $("#currentDay");
+  //setting up a date variable that is the day month year and time 
   var date = dayjs().format("dddd, MMMM D, YYYY h:mm A");
+  //replacing pdate with the text of date which is the day and time 
   pDate.text(date);
-
+//this will set a  variable under currenHour that displays the current hour 
   var currentHour = dayjs().format("H");
 
+  //setting up a for loop 
   for (let i = 9; i < 18; i++) {
+    //setting up a variable of timeblock which equals to the body divs hour class plus i with i being 9
     var timeBlock = $("#hour-" + i);
+    // setting up a variable that if i is less then the current hour 
     if (i < currentHour) {
+    //making it so if the current hour is greater then the index of the timeblock then it will have the past id 
       timeBlock.addClass("past");
+      //otheriwse if its equal to the currenthour then it will get the present class
     } else if (i == currentHour) {
       timeBlock.addClass("present");
+      //everything else gets the future 
     } else {
       timeBlock.addClass("future");
     }
